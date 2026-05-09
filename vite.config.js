7fccf82762a2d40996b62f57bbe6fb51762c7bab
Abdelrahman <119765066+Stolir@users.noreply.github.com>
2026-05-09 20:50:31 +0300

    Setup file structure and Vite config

---
+++ vite.config.js
@@ -0,0 +1,50 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "node:path";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    port: 5173,
+    strictPort: true,
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+      },
+      "/auth": {
+        target: "http://localhost:3000",
+        bypass: (req) => {
+          // Only proxy if path is exactly /auth or starts with /auth/
+          if (req.url === "/auth" || req.url.startsWith("/auth/")) {
+            return; // proxy
+          }
+          return req.url; // bypass proxy
+        },
+        changeOrigin: true,
+
+        configure: (proxy, _options) => {
+          proxy.on("error", (err, _req, _res) => {
+            console.log("proxy error", err);
+          });
+          proxy.on("proxyReq", (proxyReq, req, _res) => {
+            console.log("Sending Request to the Target:", req.method, req.url);
+          });
+          proxy.on("proxyRes", (proxyRes, req, _res) => {
+            console.log(
+              "Received Response from the Target:",
+              proxyRes.statusCode,
+              req.url,
+            );
+          });
+        },
+      },
+    },
+  },
+});
