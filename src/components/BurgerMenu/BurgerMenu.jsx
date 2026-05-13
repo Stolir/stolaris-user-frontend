41385e8f0e0af90db0d4c8c53c2c0517d3857dd5
Abdelrahman <119765066+Stolir@users.noreply.github.com>
2026-05-13 10:45:12 +0300

    Build a focus-trap component to replace npm package used on burger menu

--- src/components/BurgerMenu/BurgerMenu.jsx
+++ src/components/BurgerMenu/BurgerMenu.jsx
@@ -2,28 +2,28 @@ import { Link } from "react-router";
 import styles from "./BurgerMenu.module.css";
 import { UserCircle } from "iconoir-react";
 import { useState } from "react";
-import { FocusTrap } from "focus-trap-react";
+import FocusTrap from "../FocusTrap/FocusTrap";
 
 function BurgerMenu({ children }) {
   const [isOpen, setIsOpen] = useState(false);
-
+  console.log(isOpen);
   function toggleMenu() {
     setIsOpen(!isOpen);
-    console.log(isOpen);
   }
 
   return (
-    <FocusTrap active={isOpen}>
+    <FocusTrap isOpen={isOpen} onClose={() => setIsOpen(false)}>
       <div className={styles.burgerMenu}>
         <button
           className={`${styles.menuButton} ${isOpen ? styles.open : ""}`}
           onClick={toggleMenu}
           type="button"
+          aria-label="Open menu"
         >
-          <span></span>
+          <span aria-hidden={true}></span>
         </button>
 
-        <aside className={styles.sidebar} inert={!isOpen} aria-hidden={true}>
+        <aside className={styles.sidebar} inert={!isOpen} aria-hidden={!isOpen}>
           <nav aria-label="Menu Navigation" className={styles.navbarContent}>
             <ul>{children}</ul>
           </nav>
---
+++ src/components/FocusTrap/FocusTrap.jsx
@@ -0,0 +1,57 @@
+import { useEffect, useRef, useState } from "react";
+import styles from "./FocusTrap.module.css";
+
+function FocusTrap({ children, isOpen, onClose, focusOnCloseRef }) {
+  const modalRef = useRef(null);
+
+  useEffect(() => {
+    if (isOpen) {
+      const container = modalRef.current;
+      const focusableElements = container.querySelectorAll(
+        'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])',
+      );
+      const firstElement = focusableElements[0];
+      const lastElement = focusableElements[focusableElements.length - 1];
+      // By default when closing the focus-trapped element, focus will return to the
+      // first element. You may pass a ref to another element if you wish focus to be
+      // moved to it instead
+      const focusOnClose = focusOnCloseRef?.current || firstElement;
+
+      function handleTabPress(e) {
+        // If the pressed key is not 'Tab' return early
+        if (e.key !== "Tab") {
+          return;
+        }
+        // If shift + tab, and on First element set focus to Last element
+        if (e.shiftKey && document.activeElement === firstElement) {
+          e.preventDefault();
+          lastElement.focus();
+        }
+        // If tab, and on Last element set focus to First element
+        else if (!e.shiftKey && document.activeElement === lastElement) {
+          e.preventDefault();
+          focusOnClose.focus();
+        }
+        return;
+      }
+
+      function handleEscapePress(e) {
+        if (e.key === "Escape") {
+          firstElement.focus();
+          onClose();
+        }
+      }
+      container.addEventListener("keydown", handleTabPress);
+      container.addEventListener("keydown", handleEscapePress);
+
+      return () => {
+        container.removeEventListener("keydown", handleTabPress);
+        container.removeEventListener("keydown", handleEscapePress);
+      };
+    }
+  }, [isOpen]);
+
+  return <div ref={modalRef}>{children}</div>;
+}
+
+export default FocusTrap;
