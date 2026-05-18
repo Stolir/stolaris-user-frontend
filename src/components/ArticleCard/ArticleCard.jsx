3fc6fb2db63e2904b211d2852ee9408410962f4c
Abdelrahman <119765066+Stolir@users.noreply.github.com>
2026-05-18 10:56:17 +0300

    Create ArticleCard component

---
+++ src/components/ArticleCard/ArticleCard.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router";
+import { getArticleDescription } from "../../lib/utils";
+import AuthorName from "../AuthorName/AuthorName";
+import ReadTime from "../ReadTime/ReadTime";
+import styles from "./ArticleCard.module.css";
+import ReadMoreButton from "../ReadMoreButton/ReadMoreButton";
+
+function ArticleCard({ article }) {
+  const description = getArticleDescription(article);
+  return (
+    <article className={styles.articleCard}>
+      <Link to={`/article/${article.slug}`}>
+        <h3>{article.title}</h3>
+      </Link>
+      <p dangerouslySetInnerHTML={description} />
+      <ReadTime time={article.readTime} />
+      <ReadMoreButton link={`/article/${article.slug}`} />
+    </article>
+  );
+}
+
+export default ArticleCard;
