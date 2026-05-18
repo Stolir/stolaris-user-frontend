c8423993796d58589d26b0c2dc4597369d0753c4
Abdelrahman <119765066+Stolir@users.noreply.github.com>
2026-05-18 10:59:27 +0300

    Add ArticleList component to HomePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,7 @@
 import { useLoaderData } from "react-router";
 import styles from "./HomePage.module.css";
 import FeaturedArticle from "../../components/FeaturedArticle/FeaturedArticle";
+import ArticlesList from "../../components/ArticlesList/ArticlesList";
 
 function HomePage() {
   const data = useLoaderData();
@@ -8,6 +9,7 @@ function HomePage() {
   return (
     <main>
       <FeaturedArticle article={data.featured} />
+      <ArticlesList articles={data.all} />
     </main>
   );
 }
