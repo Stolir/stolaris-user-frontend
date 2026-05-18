95e1f860d4186e3310f136a1f41fbb3e5142a286
Abdelrahman <119765066+Stolir@users.noreply.github.com>
2026-05-18 10:23:01 +0300

    Create function to get article description

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,3 +1,5 @@
+import { createArticleHTML } from "./tiptapUtils";
+
 export function formatDateUTC(iso) {
   return iso.split("T")[0];
 }
@@ -12,4 +14,13 @@ export function formatDateLocal(iso) {
     "-" +
     String(date.getDate()).padStart(2, "0")
   );
+}
+
+export function getArticleDescription(article) {
+  const content = article.content.content;
+  // Find first paragraph and use it for the description
+  const firstPara = content.find((item) => item.type === "paragraph").content;
+  const desc = createArticleHTML(firstPara);
+
+  return desc;
 }
