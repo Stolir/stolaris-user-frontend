003ccd1a46ca36514e03fdf3fcbfab68118ff003
Abdelrahman <119765066+Stolir@users.noreply.github.com>
2026-05-19 14:30:20 +0300

    Add optional prop to AuthorName component to render a second dash

--- src/components/AuthorName/AuthorName.jsx
+++ src/components/AuthorName/AuthorName.jsx
@@ -1,10 +1,11 @@
 import styles from "./AuthorName.module.css";
 
-function AuthorName({ author }) {
+function AuthorName({ author, extraDash = false }) {
   return (
     <p className={styles.authorName}>
       <span className={styles.dash}></span>
       By {author}
+      {extraDash && <span className={styles.dash}></span>}{" "}
     </p>
   );
 }
